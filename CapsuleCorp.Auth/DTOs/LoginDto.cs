db8ca8181ff223eb5d7b908e5f04c83b29c41fc4
Eduardo Freitas <eduardo.freitas@unesp.br>
2026-04-15 01:43:17 -0300

    feat: adicionar objetos de transferência para Login

---
+++ CapsuleCorp.Auth/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapsuleCorp.Auth.DTOs
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public required string Password { get; set; }
+    }
+}
